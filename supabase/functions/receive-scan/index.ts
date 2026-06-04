7bd44bb917bf692ebc54d5b4ec866addf47580ca
YourGitHubUsername <your@email.com>
2026-06-04 10:39:39 +0530

    Final

---
+++ supabase/functions/receive-scan/index.ts
@@ -0,0 +1,28 @@
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+
+serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      },
+    });
+  }
+
+  return new Response(
+    JSON.stringify({
+      success: true,
+      message: "Receive scan function is working",
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+});
